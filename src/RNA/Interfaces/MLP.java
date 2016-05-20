d43a435b45263000dca354f954de71fcd05f9d42
castroDAM <douglas.amc@hotmail.com>
2016-05-20 14:14:39 -0300

    Montando a Estrutura MLP - setInput getOutput Ainda falta tratar a questão dos pesos Sinápticos

--- src/RNA/Interfaces/MLP.java
+++ src/RNA/Interfaces/MLP.java
@@ -18,32 +18,37 @@ public class MLP implements NeuronioInterface{
     ArrayList<Camada> listOfCamadas = new ArrayList<>();
     
     private final int numEntradas;
-    private int numCamadas;
-    private int[] numNeuronioCamada;
+    private final int numSaidas;
+    private final int numCamadas;
+    private final int[] numNeuronioCamada;
 
     public MLP(int numEntradas, int numCamadas, int[] numNeuronioCamada, ArrayList<TypeFunct> FcnPorCamada) {
         this.numEntradas = numEntradas;
         this.numCamadas = numCamadas;
         this.numNeuronioCamada = numNeuronioCamada;
-
+        this.numSaidas = numNeuronioCamada[numCamadas-1];
+        
         this.listOfCamadas.add(new Camada(this.numEntradas, this.numNeuronioCamada[0], FcnPorCamada.get(0)));
 
-        for (int i = 1; i < numCamadas; i++) {
+        for (int i = 1; i < this.numCamadas; i++) {
             this.listOfCamadas.add(new Camada(this.listOfCamadas.get(i - 1).getNumNeuronio(), this.numNeuronioCamada[i], FcnPorCamada.get(i)));
         }
     }
     
-    
-    
-    
     @Override
     public void setInputs(double[] entradas) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        double[] entrada = entradas;
+        
+        for(int i=0; i<numCamadas; i++){
+            this.listOfCamadas.get(i).setInputs(entrada);
+            entrada = this.listOfCamadas.get(i).getOutput();
+        }
     }
 
     @Override
     public double[] getOutput() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.listOfCamadas.get(this.numCamadas-1).getOutput();
     }
     
 }
